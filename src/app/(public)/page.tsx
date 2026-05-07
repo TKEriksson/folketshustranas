5310f1b6fad43387d2b59b3298918188b89e60e5
Tobias Eriksson <hungatobbe@gmail.com>
2026-05-07 22:15:00 +0200

    Sanity and nextjs setup

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="w-full h-full p-5 text-center bg-slate-600 text-white text-2xl">
+      <div className="font-bold animate-bounce">Folkets hus Tranås!</div>
+      <div className="my-5">
+        <div className="w-10 h-10 rounded-full border-3 border-blue-600 border-t-blue-300 animate-spin mx-auto"></div>
+      </div>
+      <div>Kommer snart 👌💻</div>
+    </div>
+  );
+}
---
+++ src/app/(sanity)/studio/layout.tsx
@@ -0,0 +1,24 @@
+import {
+  // metadata as studioMetadata,
+  viewport as studioViewport,
+} from "next-sanity/studio";
+
+export const dynamic = "force-static";
+
+// Här exporterar vi metadata och viewport för admin-panelen
+// export const metadata = {
+//   ...studioMetadata,
+//   title: "Folketshustranas.se - Admin",
+// };
+
+export const viewport = {
+  ...studioViewport,
+};
+
+export default function StudioLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
