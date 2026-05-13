60ba94c2320113ca0018da42e319c53eefd571a4
Tobias Eriksson <hungatobbe@gmail.com>
2026-05-13 13:04:23 +0200

    A temporary "public" site, and just playing around with sanity (learning mode)

--- src/app/(sanity)/studio/layout.tsx
+++ src/app/(sanity)/studio/layout.tsx
@@ -1,19 +1,20 @@
-import {
-  // metadata as studioMetadata,
-  viewport as studioViewport,
-} from "next-sanity/studio";
+// import {
+//   // metadata as studioMetadata,
+//   viewport as studioViewport,
+// } from "next-sanity/studio";
 
 export const dynamic = "force-static";
 
 // Här exporterar vi metadata och viewport för admin-panelen
+
 // export const metadata = {
 //   ...studioMetadata,
 //   title: "Folketshustranas.se - Admin",
 // };
 
-export const viewport = {
-  ...studioViewport,
-};
+// export const viewport = {
+//   ...studioViewport,
+// };
 
 export default function StudioLayout({
   children,
---
+++ src/sanity/schemaTypes/projekt.ts
@@ -0,0 +1,46 @@
+import { ProjectsIcon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+
+export const project = defineType({
+  name: "project",
+  title: "Project",
+  type: "document",
+  icon: ProjectsIcon,
+  fields: [
+    defineField({
+      name: "Namn",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "name",
+      },
+    }),
+    defineField({
+      name: "url",
+      title: "URL",
+      type: "url",
+    }),
+    defineField({
+      name: "desc",
+      type: "blockContent",
+      title: "Description",
+    }),
+    defineField({
+      name: "mainImage",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        defineField({
+          name: "alt",
+          title: "Alt text",
+          type: "text",
+        }),
+      ],
+    }),
+  ],
+});
