938afe633f2884c6def68146d60a6d943ec9fb3b
Tobias Eriksson <hungatobbe@gmail.com>
2026-05-17 11:24:35 +0200

    Fix app root and Sanity studio routes

--- src/app/(sanity)/studio/[[...tool]]/page.tsx
+++ src/app/(sanity)/studio/[[...tool]]/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../../../sanity.config";
+import StudioPageClient from "./StudioPageClient";
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
@@ -11,10 +8,10 @@ import config from "../../../../../sanity.config";
  * https://github.com/sanity-io/next-sanity
  */
 
-// export const dynamic = "force-static"; Flyttaat till layout.
+export const dynamic = "force-static";
 
-// export { metadata, viewport } from "next-sanity/studio"; flyttat denna till layout.
+export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return <StudioPageClient />;
 }
